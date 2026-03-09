6dc9cb39c427ed484825b51090b0b49154ad7c03
yel1337 <daniel.salvaloza2002@gmail.com>
2026-03-09 18:54:15 +0800

    corresponds with moving contact on main

--- relay/switch/close.py
+++ relay/switch/close.py
@@ -1,6 +1,5 @@
-from relay.main import Main as m
-
 def close():
+	from main import Main as m
 	com_signal = m.com
 	if com_signal == 1:
 		return True # It should always be 1 else NO is activated
--- relay/switch/open.py
+++ relay/switch/open.py
@@ -1,6 +1,5 @@
-from relay.main import Main as m
-
 def open():
+	from main import Main as m
 	com_signal = m.com
 	if com_signal == 0:
 		return False # It should always be 0 else NC is activated
