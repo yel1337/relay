b1aba77028b8888370464f6ffb6e183728b0c4d4
yel1337 <daniel.salvaloza2002@gmail.com>
2026-03-09 18:55:49 +0800

    removed unwanted imports causing circular imports

--- relay/magnet/coil.py
+++ relay/magnet/coil.py
@@ -1,23 +1,19 @@
-from relay.main import CurrentException 
-
 class ElectroMagnet(): 
 	def __init__ (self, v, r):
 		self.v_value = v
 		self.r_value = r
 
-	def current_measurement(self):
-		I = self.v_value / self.r_value
-		
+	def current_measurement(self, v, r):
 		try:
-			if I > 1 and I != 0:
-				return I
+			from main import CurrentException
+			if v > 1 and r > 1:
+				return v / r
 			else:
 				raise CurrentException("amps shoudn't be equal to 0")
 		except CurrentException as e:
 			pass
 
-	def gen_magnetic(self):
-		current = self.current_measurement()
+	def gen_magnetic(self, current):
 		if current:
 			return True
 		else:
